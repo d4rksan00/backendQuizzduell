bdd96bf927030cb76f33273ceac63d8407bb6f64
d4rksan00 <jonas.edler@compusafe.de>
2024-04-26 11:17:47 +0200

    Added Springbootfiles

---
+++ quizzapp-backend/src/main/java/org/example/quizzappbackend/QuizzappBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.quizzappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzappBackendApplication.class, args);
+	}
+
+}
---
+++ quizzapp-backend/src/test/java/org/example/quizzappbackend/QuizzappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.quizzappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
