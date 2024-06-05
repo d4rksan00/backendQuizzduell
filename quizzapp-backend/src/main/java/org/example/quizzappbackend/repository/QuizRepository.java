b5f4202eaaaaca62a80c92121f99a3535263df6e
Viktor <viktor.koevesdi@compusafe.de>
2024-06-05 13:58:24 +0200

    Commited basics

--- quizzapp-backend/src/main/java/org/example/quizzappbackend/repository/QuizRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.quizzappbackend.repository;
-
-import org.example.quizzappbackend.entity.Quiz;
-import org.springframework.data.repository.CrudRepository;
-
-public interface QuizRepository extends CrudRepository<Quiz, Long> {
-}
