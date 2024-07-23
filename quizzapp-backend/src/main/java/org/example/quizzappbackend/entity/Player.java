a99816acf3872d8923de7cdd27e8d2bab22a66b2
d4rksan00 <jonas.edler@compusafe.de>
2024-07-23 11:40:38 +0200

    set email unique

--- quizzapp-backend/src/main/java/org/example/quizzappbackend/entity/Player.java
+++ quizzapp-backend/src/main/java/org/example/quizzappbackend/entity/Player.java
@@ -19,6 +19,7 @@ public class Player {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(unique=true)
     private String email;
     private String password;
     private Boolean activePlayer;
