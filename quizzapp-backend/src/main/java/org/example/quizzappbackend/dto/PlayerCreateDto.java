58738c7623bcfcd150874909f8383224fd0fa7d4
Lavinia Berger <extern.berger_lavinia@allianz.de>
2024-07-26 14:21:02 +0200

    added sign-up function to frontend

--- quizzapp-backend/src/main/java/org/example/quizzappbackend/dto/PlayerCreateDto.java
+++ quizzapp-backend/src/main/java/org/example/quizzappbackend/dto/PlayerCreateDto.java
@@ -1,10 +1,16 @@
 package org.example.quizzappbackend.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class PlayerCreateDto {
 
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @NotBlank(message = "Password is mandatory")
     private String password;
 }
